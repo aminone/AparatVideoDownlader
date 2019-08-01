715171e092c65148d467dfc8aff4a500d53a8f93
Amin Mahmudian <amin@mahmudian.ir>
2019-08-01 08:59:41 +0430

    update to handle new html tags

--- videoDownloader.sh
+++ videoDownloader.sh
@@ -7,7 +7,7 @@
 # ./videoDownloader.sh <PAGES_LINK_LIST_FILE> #will download all the listed video links with default quality (720p)
 # ./videoDownloader.sh <PAGES_LINK_LIST_FILE> 144 #will download all the listed video links with specified quality
 ###
-
+aparatBaseUrl="https://www.aparat.com"
 tempHtmlFile="/tmp/aparatPage.html"
 videoLinksFile="videoLinks"
 downloadLinksFile="downloadReadyLinks"
@@ -28,24 +28,31 @@ fi
 
 if [[ $1 == https* ]]; then
     wget -O ${tempHtmlFile} $1 >/dev/null 2>&1
-    < ${tempHtmlFile} tr -d '\n' | grep -oP '(?<=<h2 class="video-item__title"> ).*?(?= </h2>)' | grep -o "<a href=\"https://www.aparat.com/v/.*</a>" | grep -o "'https://www\.aparat.com/v/.*'" | awk '!x[$0]++' | cut -d "'" -f2 > ${videoLinksFile}
-    chanelName=`grep -o "<title>.*</title>" ${tempHtmlFile} | sed 's/\(<title>\|<\/title>\)//g' | sed 's/\ /_/g'`
+    < ${tempHtmlFile} tr -d '\n' | grep -oP '(?<=<h2 class="thumb-title"> ).*?(?= </h2>)' | grep -o "<a href=\"/v/.*</a>" | grep -o "'/v/.*'" | awk '!x[$0]++' | cut -d "'" -f2 > ${videoLinksFile}
+    chanelName=`awk -vRS="</title>" '/<title>/{gsub(/.*<title>|\n+/,"");print;exit}' ${tempHtmlFile} | tr -d '[:space:]'`
 else
     videoLinksFile=$1
-    chanelName="$(echo $1 | sed 's/\ /_/g')"_videos
+    chanelName="$(echo $1 | tr -d '[:space:]')"_videos
 fi
 
 index=1
 if [[ -f "$videoLinksFile" ]]; then
     while IFS='' read -r line || [[ -n "$line" ]]; do
-        echo
-        echo "link #$index($line) is processing..."
 
-        wget -O ${tempHtmlFile} ${line} >/dev/null 2>&1
+        if [[ $line == $aparatBaseUrl* ]]; then
+            videoPageLink=$line
+        else
+            videoPageLink=$aparatBaseUrl$line
+        fi
+
+        echo
+        echo "link $index($videoPageLink) is processing..."
+
+        wget -O ${tempHtmlFile} ${videoPageLink} >/dev/null 2>&1
 
         LINK=`grep -o "<a href=\"https:\/\/.*-${videoQuality}p__.*\.mp4" ${tempHtmlFile} | grep -o "https.*$"`
         if [[ -z "$LINK" ]]; then
-            echo -e "\e[31mSomething went wrong! probably $line does not have any \e[4m${videoQuality}p\e[0m \e[31mversion\e[0m"
+            echo -e "\e[31mSomething went wrong! probably $videoPageLink does not have any \e[4m${videoQuality}p\e[0m \e[31mversion\e[0m"
             index=$(( $index + 1 ))
             continue
         fi
